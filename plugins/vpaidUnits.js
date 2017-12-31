519bb9bea5c5e2bbd170ab393eaaff1fba9c2b9d
Alex <alex.milkis@kaltura.com>
2017-12-31 18:58:31 +0200

    Added additional VPAID units

--- plugins/vpaidUnits.js
+++ plugins/vpaidUnits.js
@@ -11,7 +11,15 @@ const vpaidUnits = {
         "adTagUrl": "http://search.spotxchange.com/vast/2.00/79391?VPAID=1&content_page_url=test.com&cb=3202405928168446&player_width=300&player_height=250"
     },
     flashTalking: {
-        "slotType": "Flashtalking mobile vast template 2.0",
+        "slotType": "Flashtalking mobile VAST template 2.0",
         "adTagUrl": "http://servedby.flashtalking.com/imp/1/31714;812030;208;xml;DailyMail;640x360VASTHTML5/?cachebuster=%%CACHEBUSTER%%"
+    },
+    innoVidVpaid: {
+        "slotType": "Innovid Sample VPAID 2.0",
+        "adTagUrl": "https://rtr.innovid.com/r1.5554946ab01d97.36996823;cb=%7Brandom_number%7D"
+    },
+    tremorVpaid: {
+        "slotType": "TremorVideo VAST 2.0 VPAID Linear",
+        "adTagUrl": "http://demo.tremorvideo.com/proddev/vast/vast2VPAIDLinear.xml"
     }
 };
