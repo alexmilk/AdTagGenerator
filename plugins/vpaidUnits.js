9b1852322960110d4faecc8e16200174e770d968
Alex <alex.milkis@kaltura.com>
2017-12-31 16:30:21 +0200

    Finished the initial layout

--- plugins/vpaidUnits.js
+++ plugins/vpaidUnits.js
@@ -3,6 +3,7 @@
  */
 const vpaidUnits = {
     spotXVpaid: {
-    "SpotX VPAID 2.0 W. Kaltura Params": "http://search.spotxchange.com/vast/2.0/85394?VPAID=JS&content_page_url={utility.referrer_url}&cb={utility.random}&player_width=REPLACE_ME&player_height=REPLACE_ME"
+    "slotType": "SpotX VPAID 2.0 W. Kaltura Params",
+    "adTagUrl": "http://search.spotxchange.com/vast/2.0/85394?VPAID=JS&content_page_url={utility.referrer_url}&cb={utility.random}&player_width=REPLACE_ME&player_height=REPLACE_ME"
     }
 };
