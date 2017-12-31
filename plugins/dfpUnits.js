9dab6bca39259285d86e92cda80b3a469c67eb88
Alex <alex.milkis@kaltura.com>
2017-12-31 12:13:46 +0200

    Additional styling has been added, ad slots prepared

--- plugins/dfpUnits.js
+++ plugins/dfpUnits.js
@@ -1,3 +1,49 @@
 /**
  * Created by alex.milkis on 27/12/2017.
  */
+const dfpSlots = {
+    inlinePreroll: {
+        "slotType": "Single Inline Linear",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=",
+    },
+    skippablePreroll: {
+        "slotType": "Single Skippable Inline",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=",
+    },
+    singleRedirectLinear: {
+        "slotType": "Single Redirect Linear",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator=",
+    },
+    singleRedirectError: {
+        "slotType": "Single Redirect Error",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirecterror&nofb=1&correlator=",
+    },
+    singleRedirectBroken: {
+        "slotType": "Single Redirect Broken (Fallback)",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirecterror&correlator=",
+    },
+    singleNonlinearInline: {
+        "slotType": "Single Non-linear Inline",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=480x70&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dnonlinear&correlator=",
+    },
+    vMAPPreRoll: {
+        "slotType": "VMAP Pre-roll",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator=",
+    },
+    vMAPPreRollBumper: {
+        "slotType": "VMAP Pre-roll + Bumper",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonlybumper&cmsid=496&vid=short_onecue&correlator=",
+    },
+    vMAPPostRoll: {
+        "slotType": "VMAP Post-roll",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpostonly&cmsid=496&vid=short_onecue&correlator=",
+    },
+    vMAPPostRollBumper: {
+        "slotType": "VMAP Post-roll + Bumper",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpostonlybumper&cmsid=496&vid=short_onecue&correlator=",
+    },
+    vMAPPreMidPostSingleAds: {
+        "slotType": "VMAP Pre-, Mid-, and Post-rolls, Single Ads",
+        "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpost&cmsid=496&vid=short_onecue&correlator=",
+    }
+};
--- plugins/vpaidUnits.js
+++ plugins/vpaidUnits.js
@@ -2,5 +2,7 @@
  * Created by alex.milkis on 27/12/2017.
  */
 const vpaidUnits = {
+    spotXVpaid: {
     "SpotX VPAID 2.0 W. Kaltura Params": "http://search.spotxchange.com/vast/2.0/85394?VPAID=JS&content_page_url={utility.referrer_url}&cb={utility.random}&player_width=REPLACE_ME&player_height=REPLACE_ME"
+    }
 };
