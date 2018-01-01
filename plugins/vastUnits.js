bb21b295c65eea7fc525e22fa0c18c2c2375c8ef
Alex <alex.milkis@kaltura.com>
2018-01-01 11:33:35 +0200

    Fixed menu containers size to become responsive

--- plugins/vastUnits.js
+++ plugins/vastUnits.js
@@ -3,7 +3,11 @@
  */
 const vastSlots = {
     inlinePreroll: {
-        "slotType": "Single ad preroll",
+        "slotType": "MediaMind Single ad preroll",
         "adTagUrl": "https://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=14602547&PluID=0&pos=9562&ord=%5Btimestamp%5D&cim=1",
     },
+    tremorVideoLinear: {
+        "slotType": "TremorVideo VAST 2.0 Regular Linear",
+        "adTagUrl": "http://demo.tremorvideo.com/proddev/vast/vast2RegularLinear.xml",
+    }
 };
