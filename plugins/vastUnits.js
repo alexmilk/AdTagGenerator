1aae8c43c7e6abf8c6c823727af6b7cbb221f360
Alex <alex.milkis@kaltura.com>
2018-03-12 11:44:48 +0200

    Added additional VAST Ad tag

--- plugins/vastUnits.js
+++ plugins/vastUnits.js
@@ -9,5 +9,9 @@ const vastSlots = {
     tremorVideoLinear: {
         "slotType": "TremorVideo VAST 2.0 Regular Linear",
         "adTagUrl": "http://demo.tremorvideo.com/proddev/vast/vast2RegularLinear.xml",
+    },
+    lkqdVastLinear: {
+        "slotType": "LKQD VAST 2.0 Video Linear Rendition",
+        "adTagUrl": "https://ssp.lkqd.net/ad?pid=105&sid=205108&output=vast&execution=any&placement=&playinit=auto&volume=0&width={WIDTH}&height={HEIGHT}&dnt={DO_NOT_TRACK}&pageurl={PAGEURL}&contentid={CONTENT_ID}&contenttitle={CONTENT_TITLE}&contentlength={CONTENT_LENGTH}&contenturl={CONTENT_URL}&rnd={CACHEBUSTER}"
     }
 };
