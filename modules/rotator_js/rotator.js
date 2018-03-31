fd8d25cb3bee55e222afd3f974cf10ab2e76ac87
Alex <alex.milkis@kaltura.com>
2018-03-31 19:37:45 +0300

    Added Rotator.js code initialization

--- modules/rotator_js/rotator.js
+++ modules/rotator_js/rotator.js
@@ -1,2 +1,17 @@
-// Copyright Alex Milkis 2018
-// MIT LICENSE
+/*!
+* Rotator.js
+* A javascript header rotator built on jQuery with extensive features and expandability.
+*
+* @author  Alex Milkis
+* @source  https://github.com/alexmilk/AdTagGenerator/
+* @copyright  MIT License
+* @version  v0.1
+*/
+
+(function ($) {
+
+	$.Rotator = function (element, options) {
+
+		this.version = '0.1';
+
+	}())
