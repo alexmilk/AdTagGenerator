fc29dac91d5278db2a26e4fa5fe32895e6eb8534
Alex <alex.milkis@kaltura.com>
2018-04-15 09:57:16 +0300

    Created additional credit links for the footer

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//The main application JS                                    ||
+//Main applications script                                   ||
 //Created by Alex Milkis - 2017                              ||
 //The MIT License                                            ||
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
+//Site ready functions and menu looping scripts.
+$(document).ready(function() {
+    $('#ribbonBeta').slideDown(900);
+    let selectors = {
+        adSlot: $("#adSlotSelector"),
+        dfpSlot: $("#dfpSlotSelector"),
+        vpaidSlot: $("#vpaidSlotSelector")
+    };
+    for (let prop in vastSlots) {
+        selectors.adSlot.append("<option value= " + prop + ">" + vastSlots[prop].slotType + "</option>")
+    }
+    for (let prop in dfpSlots) {
+        selectors.dfpSlot.append("<option value= " + prop + ">" + dfpSlots[prop].slotType + "</option>")
+    }
+    for (let prop in vpaidUnits) {
+        selectors.vpaidSlot.append("<option value= " + prop + ">" + vpaidUnits[prop].slotType + "</option>")
+    }
+    $('#topLeftTitle').slideDown(900);
+    $('#bottomCredit').slideDown(900);
+});
+//AdTag copy function
 copyAdTag = () => {
     let copyText = document.getElementById("adTagId");
     copyText.select();
